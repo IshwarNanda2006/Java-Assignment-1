447dcc206607d82f1a2d55da6b3302dc66023033
IshwarNanda2006 <nandaishwar30@gmail.com>
2026-08-29 00:19:52 +0530

    Add assignment question 2

---
+++ Tut1level1q1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Tut1level1q1 {
+
+    public static void checkVotingEligibility(int age) {
+
+        if (age >= 18) {
+            System.out.println("Eligible to vote");
+        } else {
+            System.out.println("Not eligible to vote");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int age = sc.nextInt();
+
+        checkVotingEligibility(age);
+
+        sc.close();
+    }
+}
